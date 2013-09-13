0a7e960a6080444e7ea703218c26ef8ff072f478
Filip W <wojcieszyn@gmail.com>
2013-09-13 11:10:28 +0200

    Updated to support cache invalidation and dependencies

--- Strathweb.CacheOutput.Azure/AzureCachingProvider.cs
+++ Strathweb.CacheOutput.Azure/AzureCachingProvider.cs
@@ -7,44 +7,57 @@ namespace Strathweb.CacheOutput.Azure
     public class AzureCachingProvider : IApiOutputCache
     {
         private readonly DataCache _cache;
+        private const string Region = "GlobalRegion";
 
         public AzureCachingProvider()
         {
             _cache = new DataCache();
+            _cache.CreateRegion(Region);
         }
 
         public void RemoveStartsWith(string key)
         {
-            _cache.Remove(key);
+            var objs = _cache.GetObjectsByTag(new DataCacheTag(key), Region);
+            foreach (var o in objs)
+            {
+                _cache.Remove(o.Key, Region);
+            }
         }
 
         public T Get<T>(string key) where T : class
         {
-            var result = _cache.Get(key) as T;
+            var result = _cache.Get(key, Region) as T;
             return result;
         }
 
         public object Get(string key)
         {
-            var result = _cache.Get(key);
+            var result = _cache.Get(key, Region);
             return result;
         }
 
         public void Remove(string key)
         {
-            _cache.Remove(key);
+            _cache.Remove(key, Region);
         }
 
         public bool Contains(string key)
         {
-            var result = _cache.Get(key);
-            return result != null;
+            var result = _cache.Get(key, Region);
+            if (result != null) return true;
+
+            return false;
         }
 
         public void Add(string key, object o, DateTimeOffset expiration, string dependsOnKey = null)
         {
             var exp = expiration - DateTime.Now;
-            _cache.Put(key, o, exp);
+            if (dependsOnKey == null)
+            {
+                dependsOnKey = key;
+            }
+
+            _cache.Put(key, o, exp, new[] { new DataCacheTag(dependsOnKey) }, Region);
         }
     }
 }
